ffdf5b9e7f51cd2f83442f723d7343b78baff264
juampaxs@hotmail.com <mlgiordano92@gmail.com>
2022-02-16 13:30:34 -0300

    correccion de lista de items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,13 @@
-import { Card } from "@material-ui/core";
-
 function Item ({ item }) {
     return (
         <div className="item-container">
-            <Card>
-                <Card.Img variant="top" src={item.pictureUrl} />
-                <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>{item.description}</Card.Text>
-                </Card.Body>
-                <Card.Footer>{item.price}</Card.Footer>
+            <article>
+                <img variant="top" src={item.pictureUrl} />
+                <p>{item.title}</p>
+                <p>{item.description}</p>
+                <p>{item.price}</p>
                 <button>Ver Detalle</button>
-            </Card>
+            </article>
         </div>
     );
 }
---
+++ src/components/ItemDetailContainer.js
@@ -0,0 +1,13 @@
+import ItemDetail from "./ItemDetail";
+
+const getItem = () => {
+
+}
+
+function ItemDetailContainer () {
+    return <div>
+        <ItemDetail></ItemDetail>
+    </div>
+}
+
+export default ItemDetailContainer;
